9406d7c972fde9eecc8372e521c52f48600ddff2
Asaad Ahmed <asaadbinahmed@gmail.com>
2024-12-16 22:19:59 -0500

    asks the user for number of evaluations, the evaluations name, and their percent, and constructs a pie graph using this info

--- app.py
+++ app.py
@@ -1,15 +1,33 @@
 import plotly.express as px
 
-mark_breakdown = {
-    "Homework": "10%",
-    "Midterm 1": "23%",
-    "Midterm 2": "22%",
-    "Final Exam": "45%",
-}
+print("################################################\n\t\tSyllabus Tool\n################################################")
+number_of_evaluations = 0
+while True:
+    try:
+        number_of_evaluations = int(input("How many evaluations are there? Enter a number (1 to 100): "))
+    except:
+        continue
+    else:
+        if 0 < number_of_evaluations <= 100: break
+        continue
 
-percents = list(map(lambda weight : int(weight.replace("%", "")), mark_breakdown.values()))
-tasks = list(mark_breakdown.keys())
-px.pie(values=percents, names=tasks).show()
+evaluations = dict()
+total_worth = 0
+counter = 1
 
-#for task, weight in mark_breakdown.items():
-#    print(task + "\t" + weight)
+# TODO: Write input validation
+for i in range(number_of_evaluations):
+    name = input("Enter the evaluation name (optional): ") or f"Evaluation_{counter}"
+    worth = int(input("Enter the percent that this evaluation is worth: "))
+    counter += 1
+    evaluations[name] = worth
+    total_worth += worth
+
+print(total_worth)
+if total_worth != 100:
+    raise Exception("Check your percents, it does not add up to 100%")
+
+print(evaluations)
+names = evaluations.keys()
+percents = list(evaluations.values())
+px.pie(values=percents, names=names).show()
